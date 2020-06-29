ca3e5aa6bef34c8edd17c51129afb3139bb68789
JZerman2018 <juwanazerman@gmail.com>
2020-06-29 06:17:19 +0000

    update run_kubernetes script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="jzerman2018/udacityapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run udacitymlapi --image=$dockerpath --port=80
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+echo "creating pod"
+kubectl port-forward udacitymlapi 8000:80
 
